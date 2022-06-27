8875ec92c4abb5f8ed398783311279af548dd738
GB <voducthang16@gmail.com>
2022-06-27 08:39:54 +0700

    add flex-grid & update scss
    27.06.2022 - 8:40 AM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './base.scss';
+import './flex-grid.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyles from '~/components/GlobalStyles';
