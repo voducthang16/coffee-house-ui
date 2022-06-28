64c488e1399fa128ed3a2a366a960fa001099280
GB <voducthang16@gmail.com>
2022-06-28 08:51:25 +0700

    update config router/layout
    28.06.2022 - 8:50 AM

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,22 @@
+import Home from '~/pages/Home';
+import About from '~/pages/About';
+import Auth from '~/pages/Auth';
 // not required login
-const publicRoutes = [];
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/about',
+        component: About,
+    },
+    {
+        path: 'auth',
+        component: Auth,
+        layout: null,
+    },
+];
 
 // required login routes
 const privateRoutes = [];
